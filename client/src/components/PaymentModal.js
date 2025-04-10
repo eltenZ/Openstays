c64e4f9140f1cafb7c02dec00f1f18543b32469c
eltenZ <shemwat04@gmail.com>
2025-04-10 16:31:51 +0300

    Update payment modal api call

--- client/src/components/PaymentModal.js
+++ client/src/components/PaymentModal.js
@@ -55,7 +55,7 @@ const sendReservation = async () => {
   };
 
   try {
-    const response = await fetch("${process.env.REACT_APP_API_BASE_URL}/api/reservations", {
+    const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/reservations`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",

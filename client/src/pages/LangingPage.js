82407a9312682b30d47f0776646484a48b253692
eltenZ <shemwat04@gmail.com>
2025-04-10 16:45:15 +0300

    update landing page background

--- client/src/pages/LangingPage.js
+++ client/src/pages/LangingPage.js
@@ -3,9 +3,13 @@ import { HeroSection } from '../components/HeroSection'
 import { SignupForm } from '../components/SignUpForm'
 import { CountdownTimer } from '../components/CountDownTimer'
 import { LoadingStatus } from '../components/LoadingStatus'
+
 export function LandingPage() {
   return (
-    <main className="min-h-screen w-full bg-gray-50 py-16 md:py-24">
+    <main
+      className="min-h-screen w-full py-16 md:py-24 bg-cover bg-center"
+      style={{ backgroundImage: "url('/images/background1.jpg')" }}
+    >
       <div className="container mx-auto space-y-16">
         <HeroSection />
         <SignupForm />
